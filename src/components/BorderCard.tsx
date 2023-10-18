2264f2aaf10846410b271098097d4ff6b01a3650
kupoapo <mckittehs@gmail.com>
2023-10-18 16:33:29 -0400

    Update kanji page

--- src/components/BorderCard.tsx
+++ src/components/BorderCard.tsx
@@ -3,11 +3,17 @@ import { PropsWithChildren, ReactNode } from 'react';
 
 interface BorderCardProps extends PropsWithChildren {
   title: ReactNode;
+  className?: string;
 }
 
-export const BorderCard: React.FC<BorderCardProps> = ({ title, children }) => {
+export const BorderCard: React.FC<BorderCardProps> = ({
+  title,
+  children,
+  className,
+  ...props
+}) => {
   return (
-    <Card border>
+    <Card border className={className} {...props}>
       <Pane padding={16} borderBottom>
         <Heading size='800'>{title}</Heading>
       </Pane>
--- src/pages/Kanji.tsx
+++ src/pages/Kanji.tsx
@@ -1,5 +1,39 @@
+import { Button, Menu, Pane } from 'evergreen-ui';
+import { BorderCard } from '../components/BorderCard';
+import { Hirigana, Katakana } from '../data/Kanji';
+
+const Alphabets = [
+  {
+    name: 'Hirigana',
+    letters: Hirigana,
+  },
+  {
+    name: 'Katakana',
+    letters: Katakana,
+  },
+];
+
 const Kanji = () => {
-  return <></>;
+  console.log('home');
+  return (
+    <div className='grid grid-cols-2 grid-flow-col gap-4'>
+      {Alphabets.map(({ name, letters }) => (
+        <BorderCard title={name}>
+          <div className='flex flex-wrap justify-center'>
+            {letters
+              .filter((h) => h.type === 'gojuuon')
+              .map((letter) => (
+                <Pane border className='w-20 p-4 text-center'>
+                  {letter.kana}
+                  <hr />
+                  {letter.roumaji}
+                </Pane>
+              ))}
+          </div>
+        </BorderCard>
+      ))}
+    </div>
+  );
 };
 
 export { Kanji };
